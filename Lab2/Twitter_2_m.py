8ea5bb968b413ae67ea7e55bf9beee972e9a4c04
Marc Garnica Caparrós <marcgarnica13@gmail.com>
2018-03-13 23:39:43 +0100

    Preparing deliverable
     - Adding results and screenshots to README.md.
     - Testing different queries and parameters for tweepy.
     - Missing last chapter 2.3: Tweet pre-processing.

--- Lab2/Twitter_2_m.py
+++ Lab2/Twitter_2_m.py
@@ -19,20 +19,20 @@ print ('Friends: ' + str(user.followers_count))
 print ('Created: ' + str(user.created_at))
 print('Description: ' + str(user.description))
 
-#'''
+'''
 # We use 1 to limit the number of tweets we are reading
 #and we only access the text of the tweet
 for status in tweepy.Cursor(api.home_timeline).items(1):
     print(status.text)
 #'''
 
-#'''
+'''
 # Print for each tweet its status with json format
-for status in tweepy.Cursor(api.home_timeline).items(5):
+for status in tweepy.Cursor(api.home_timeline).items(1):
     print(json.dumps(status._json, indent=2))
 #'''
 
-#'''
+'''
 # Print a list of 10 of our friends
 for friend in tweepy.Cursor(api.friends).items(10):
     print(json.dumps(friend._json, indent=2))
