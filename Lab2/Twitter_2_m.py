f8ab342807f8320f7a98bcaa846c13bb010dc646
Marc Garnica Caparrós <marcgarnica13@gmail.com>
2018-03-13 22:42:04 +0100

    Task 2.2 twitter accessing tweets info

--- Lab2/Twitter_2_m.py
+++ Lab2/Twitter_2_m.py
@@ -19,27 +19,27 @@ print ('Friends: ' + str(user.followers_count))
 print ('Created: ' + str(user.created_at))
 print('Description: ' + str(user.description))
 
-"""
-We use 1 to limit the number of tweets we are reading
-and we only access the text of the tweet
-"""
+#'''
+# We use 1 to limit the number of tweets we are reading
+#and we only access the text of the tweet
 for status in tweepy.Cursor(api.home_timeline).items(1):
     print(status.text)
+#'''
 
-"""
+#'''
 # Print for each tweet its status with json format
-for status in tweepy.Cursor(api.home_timeline).items(1):
-    print(json.dumps(status._json, indent=2)
-"""
+for status in tweepy.Cursor(api.home_timeline).items(5):
+    print(json.dumps(status._json, indent=2))
+#'''
 
-"""
+#'''
 # Print a list of 10 of our friends
 for friend in tweepy.Cursor(api.friends).items(10):
     print(json.dumps(friend._json, indent=2))
-"""
+#'''
 
-"""
+#'''
 # Print some of our tweets
 for tweet in tweepy.Cursor(api.user_timeline).items(1):
     print(json.dumps(tweet._json, indent=2))
-"""
+#'''
