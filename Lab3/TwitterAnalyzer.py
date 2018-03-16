a0dfcb53a3140a47c269a54f474d02aeb0c85489
Marc Garnica Caparrós <marcgarnica13@gmail.com>
2018-03-16 10:02:57 +0100

    Starting case study, reading hashtags list

--- Lab3/TwitterAnalyzer.py
+++ Lab3/TwitterAnalyzer.py
@@ -1,38 +1,46 @@
 import json
-from collections import Counter
 import nltk
-from nltk.corpus import stopwords
-nltk.download("stopwords") # download the stopword corpus on our computer
 import string
+
 from Lab2.Twitter_2 import preprocess
 
-punctuation = list(string.punctuation)
-stop = stopwords.words('english') + punctuation + ['rt', 'via', 'RT', '…']
+from collections import Counter
+from nltk.corpus import stopwords
+nltk.download("stopwords")
 
-fname = 'ArtificialIntelligenceTweets.json'
-with open(fname, 'r') as f:
+punctuation = list(string.punctuation)
+stop = punctuation + stopwords.words('spanish') + stopwords.words('english') + ['rt', 'via', '…', 'RT', '‘'] + ['ón', 'és', 'i', 'ó', '', 'els']
+
+
+with open('AnalyticsTweets.json', 'r') as json_file:
     count_all = Counter()
-    count_hashtag = Counter()
-    count_term = Counter()
-    for line in f:
+    count_stop = Counter()
+    count_hash = Counter()
+    count_mentions = Counter()
+
+    for line in json_file:
         tweet = json.loads(line)
-        # Create a list with all the terms
-        terms_stop = [term for term in preprocess(tweet['text'])
-                      if term not in stop]
-        hashtag_only = [term for term in preprocess(tweet['text'])
-                        if term.startswith('#')]
-        terms_only = [term for term in preprocess(tweet['text'])
-                      if term not in stop and
-                      not term.startswith(('#', '@'))]
-        count_all.update(terms_stop)
-        count_hashtag.update(hashtag_only)
-        count_term.update(terms_only)
-    print('Top 10 tokens: ', end=" ")
-    for word, index in count_all.most_common(10):
-        print (word, end="\t")
-    print('\nTop 10 hashtags: ', end=" ")
-    for word, index in count_hashtag.most_common(10):
-        print (word, end="\t")
-    print('\nTop 10 terms: ', end=" ")
-    for word, index in count_term.most_common(10):
-        print (word, end="\t")
+
+        # List of all the terms
+        t_all = [term for term in preprocess(tweet['text'])]
+
+        # List of all the terms without sw and it is not a hashtag
+        t_stop = [word for word in preprocess(tweet['text']) if word not in stop and not word.startswith(('#', '@'))]
+
+        # List of all hashtags
+        t_hash = [hash for hash in preprocess(tweet['text']) if hash.startswith('#')]
+
+        # Lis of all mentions
+        t_mention = [mention for mention in preprocess(tweet['text']) if mention.startswith('@')]
+
+        # Update the counter
+        count_all.update(t_all)
+        count_stop.update(t_stop)
+        count_hash.update(t_hash)
+        count_mentions.update(t_mention)
+
+    print("-------RESULT-------")
+    print("Top 10 token appearences -> " + str(count_all.most_common(10)))
+    print("Top 10 word appearences (without stopwords) -> " + str(count_stop.most_common(10)))
+    print("Top 10 hashtag appearences -> " + str(count_hash.most_common(10)))
+    print("Top 10 mention appearences -> " + str(count_mentions.most_common(10)))
