63d5789f984d86a1af39b562224c00b6ae7b3cdd
Marc Garnica Caparrós <marcgarnica13@gmail.com>
2018-04-24 19:39:51 +0200

    Finalizing optional part of lab6
     - Getting the images with a interactive input interface through the terminal
     - Twitter keys and google cloud credentials need to be added to the environment.
     - Next: Making a word cloud with the results.
     - Next: Try to send all the images at once to the Google Cloud Vision api.

---
+++ Lab6o/ImageAnalyzer.py
@@ -0,0 +1,85 @@
+import os
+import tweepy
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import googleapiclient.discovery
+
+
+from tweepy import OAuthHandler
+from collections import Counter
+
+auth = OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_SECRET'])
+
+api = tweepy.API(auth)
+
+def main(twitter_profile, numberOfImages):
+    print(' -- Receiving a new request')
+    print(' -- Twitter URL: ' + twitter_profile)
+    # The only parameter is the twitter profile url
+    profile_name = twitter_profile.rsplit('/', 1)[1]
+    print(' -- Twitter user: ' + profile_name)
+    print(' -- (1) Getting the last ' + str(numberOfImages) + ' photos posted')
+
+    image_count = 0
+    tweet_count = 0
+
+    t_images = []
+
+    while (image_count < numberOfImages):
+        tweets = api.user_timeline(profile_name, count=100)
+        for t in tweets:
+            if (image_count < numberOfImages):
+                tweet_count += 1
+            try:
+                for m in t.entities['media']:
+                    if m['type'] == 'photo':
+                        t_images.append(m['media_url_https'])
+                        image_count += 1
+            except:
+                pass
+    print(' -- ' + str(image_count) + ' pictures were contained in his/her/its last ' + str(tweet_count) + ' tweets')
+
+    print(' -- Analysing the pictures with Google Cloud Vision API')
+    service = googleapiclient.discovery.build('vision', 'v1')
+    count_label = Counter()
+    for url in t_images:
+        service_request = service.images().annotate(body={
+            'requests': [{
+                'image': {
+                    'source': {
+                        "imageUri": url
+                    }
+                },
+                'features': [{
+                    'type': 'LABEL_DETECTION',
+                    'maxResults': 10
+                }]
+            }]
+        })
+
+        # [END construct_request]
+        # [START parse_response]
+        response = service_request.execute()
+        print("Results for image " + url)
+        for result in response['responses'][0]['labelAnnotations']:
+            #print("%s - %.3f" % (result['description'], result['score']))
+            count_label[result['description']] += result['score']
+        # [END parse_response]
+
+    print(str(count_label.most_common()))
+
+    mpl.rcParams['figure.figsize'] = (6,6)
+    sorted_x, sorted_y = zip(*count_label.most_common(10))
+    print(sorted_x, sorted_y)
+
+    plt.bar(range(len(sorted_x)), sorted_y, width=0.75, align='center')
+    plt.xticks(range(len(sorted_x)), sorted_x, rotation=60)
+    plt.axis('tight')
+
+    plt.show()
+
+
+twitter_url = str(input("Introduce the twitter url: "))
+numberOfImages = int(input("Introduce the number of images for the analysis: "))
+main(twitter_url, numberOfImages)
