a737e6f6706355455894cbca4c301519a9abb787
Marc Garnica Caparrós <marcgarnica13@gmail.com>
2018-04-25 23:09:20 +0200

    Finishing Lab6o results and starting the documentation.
     - Added wordcloud generation.
     - Saving histogram and wordcloud in local files.
     - Fixed a bug on counting the images.
     - TODO: Batch processing of images by Google Cloud Vision API.

--- Lab6o/ImageAnalyzer.py
+++ Lab6o/ImageAnalyzer.py
@@ -7,11 +7,39 @@ import googleapiclient.discovery
 
 from tweepy import OAuthHandler
 from collections import Counter
+from wordcloud import WordCloud
 
 auth = OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
 auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_SECRET'])
 
 api = tweepy.API(auth)
+
+def generate_histogram(count_label, name):
+    print(str(count_label.most_common()))
+
+    mpl.rcParams['figure.figsize'] = (6, 6)
+    sorted_x, sorted_y = zip(*count_label.most_common(10))
+    print(sorted_x, sorted_y)
+
+    plt.bar(range(len(sorted_x)), sorted_y, width=0.75, align='center')
+    plt.xticks(range(len(sorted_x)), sorted_x, rotation=60)
+    plt.axis('tight')
+    plt.savefig(name + '_histogram.png')
+
+    plt.show()
+
+    print(" -- Histogram saved in " + name + "_histogram.png file.")
+
+def generate_wordcloud(count_label, name):
+    count_label = dict(count_label)
+    wordcloud = WordCloud().generate_from_frequencies(count_label)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.savefig(name + '_wordcloud.png')
+
+    plt.show()
+
+    print(" -- Wordcloud saved in " + name + "wordcloud.png file.")
 
 def main(twitter_profile, numberOfImages):
     print(' -- Receiving a new request')
@@ -19,7 +47,7 @@ def main(twitter_profile, numberOfImages):
     # The only parameter is the twitter profile url
     profile_name = twitter_profile.rsplit('/', 1)[1]
     print(' -- Twitter user: ' + profile_name)
-    print(' -- (1) Getting the last ' + str(numberOfImages) + ' photos posted')
+    print(' -- Getting the last ' + str(numberOfImages) + ' photos posted')
 
     image_count = 0
     tweet_count = 0
@@ -31,19 +59,22 @@ def main(twitter_profile, numberOfImages):
         for t in tweets:
             if (image_count < numberOfImages):
                 tweet_count += 1
-            try:
-                for m in t.entities['media']:
-                    if m['type'] == 'photo':
-                        t_images.append(m['media_url_https'])
-                        image_count += 1
-            except:
-                pass
+                try:
+                    for m in t.entities['media']:
+                        if m['type'] == 'photo':
+                            t_images.append(m['media_url_https'])
+                            image_count += 1
+                except:
+                    pass
+
     print(' -- ' + str(image_count) + ' pictures were contained in his/her/its last ' + str(tweet_count) + ' tweets')
 
     print(' -- Analysing the pictures with Google Cloud Vision API')
+    image_count = 0
     service = googleapiclient.discovery.build('vision', 'v1')
     count_label = Counter()
     for url in t_images:
+        image_count += 1
         service_request = service.images().annotate(body={
             'requests': [{
                 'image': {
@@ -61,23 +92,16 @@ def main(twitter_profile, numberOfImages):
         # [END construct_request]
         # [START parse_response]
         response = service_request.execute()
-        print("Results for image " + url)
+        print("Results for image " + url + " " + image_count + " out of " + numberOfImages)
         for result in response['responses'][0]['labelAnnotations']:
             #print("%s - %.3f" % (result['description'], result['score']))
             count_label[result['description']] += result['score']
         # [END parse_response]
 
-    print(str(count_label.most_common()))
+    generate_histogram(count_label, profile_name)
+    generate_wordcloud(count_label, profile_name)
 
-    mpl.rcParams['figure.figsize'] = (6,6)
-    sorted_x, sorted_y = zip(*count_label.most_common(10))
-    print(sorted_x, sorted_y)
 
-    plt.bar(range(len(sorted_x)), sorted_y, width=0.75, align='center')
-    plt.xticks(range(len(sorted_x)), sorted_x, rotation=60)
-    plt.axis('tight')
-
-    plt.show()
 
 
 twitter_url = str(input("Introduce the twitter url: "))
---
+++ Lab6o/README.md
@@ -0,0 +1,20 @@
+# Lab session #6: Interacting with users and services in the Cloud
+
+### Universitat Politècnica de Catalunya
+
+Course link: [ccbda-upc.github.io](https://ccbda-upc.github.io/).
+
+Group 1207
+
+-   Quang Duy Tran - duy9968\@gmail.com
+
+-   Marc Garnica Caparros - marcgarnicacaparros\@gmail.com
+
+Assignment link: [Lab 6 Assignment](https://github.com/CCBDA-UPC/Assignments-2018/blob/master/Lab06.md)
+Webapp repository: [Django express webapp](https://github.com/marcgarnica13/eb-django-express-singup-base)
+
+## Objectives
+
+This lab is the followup on the lab 6 session.
+
+### Task 6.3: Advanced Analytics as a Service in the Cloud
